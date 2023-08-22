7b0e3a8a1ae99850784bd1d4eac4ca9216026209
St IPS <114295873+soportesaludtolimaips@users.noreply.github.com>
2023-08-22 14:05:20 -0500

    Update transcribirEstudioComponent.vue

--- resources/js/components/modulos/estudios/transcribirEstudioComponent.vue
+++ resources/js/components/modulos/estudios/transcribirEstudioComponent.vue
@@ -399,8 +399,8 @@ export default {
                         stack: 6,
                     });
 
-                    /* this.listarPendientesTrascribir()
-                    this.btnCerralModalForm(); */
+                    this.listarPendientesTrascribir()
+                    this.btnCerralModalForm();
                 }
             } catch (error) {
                 console.log(error);
