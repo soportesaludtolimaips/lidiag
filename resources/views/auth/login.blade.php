1cc372cc96983ed4df94ebee0fe0321a5a946d78
St IPS <114295873+soportesaludtolimaips@users.noreply.github.com>
2024-03-06 13:47:20 -0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -69,7 +69,7 @@
 
                     <div class="form-group m-t-40">
                         <div class="col-xs-12">
-                            <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email', 'administrador@administrador.com') }}" required autocomplete="email" autofocus placeholder="Email">
+                            <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email', '') }}" required autocomplete="email" autofocus placeholder="Email">
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -79,7 +79,7 @@
                     </div>
                     <div class="form-group m-t-40">
                         <div class="col-xs-12">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Contraseña" value="123456">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Contraseña" value="">
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
