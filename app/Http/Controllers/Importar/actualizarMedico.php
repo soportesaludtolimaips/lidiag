35a3c21c4bb0e6e3b1ded0649b5f752532ae1ae0
St IPS <114295873+soportesaludtolimaips@users.noreply.github.com>
2024-03-07 15:25:25 -0500

    Update actualizarMedico.php

--- app/Http/Controllers/Importar/actualizarMedico.php
+++ app/Http/Controllers/Importar/actualizarMedico.php
@@ -43,12 +43,16 @@ class ActualizarMedico extends Controller
              * Busco el medico en Lidiag
              */
             Config::set('database.default', 'mysql');
-            $medicoLidiag = User::where('num_docu', '=', '80082041')
+            $medicoLidiag = User::where('num_docu', '=', $medicoDicom[0]->num_docu)
                 ->get();
 
-            $estudio = Estudio::where('study_pk', '=', $agenda->study_pk);
-            $estudio->medico_id = $medicoLidiag[0]->id;
-            $estudio->save();
+            $estudio = Estudio::where('study_pk', '=', $agenda->study_pk)->get();
+            if ($estudio) {
+                $estudio->medico_id = $medicoLidiag[0]->id;
+                $estudio->save();
+
+                echo "Atualizado estudio " . $estudio->id . ", el medico actual es: " . $medicoDicom->nom . " " . $medicoDicom->nom . ", el nuevo medico es: " . $medicoLidiag[0]->name;
+            }
         }
     }
 }
